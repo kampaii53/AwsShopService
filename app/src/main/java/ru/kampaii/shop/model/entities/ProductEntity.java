4d0d4106cc6cf5b812f9498d6e520d5fbaf1a968
Kirill_Kuznetsov1 <Chfysqgfhjkm53>
2021-08-14 12:54:30 +0500

    commit to test jenkins trigger

--- app/src/main/java/ru/kampaii/shop/model/entities/ProductEntity.java
+++ app/src/main/java/ru/kampaii/shop/model/entities/ProductEntity.java
@@ -5,6 +5,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+/**
+ * Entity of a product
+ */
 @Entity
 public class ProductEntity {
 
