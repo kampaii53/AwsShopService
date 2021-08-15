f29f89458c889ca641e1032f37404e3c5cf789cd
Kirill_Kuznetsov1 <Chfysqgfhjkm53>
2021-08-15 14:51:18 +0500

    jenkinsfile edited

--- jenkins/BuildAndPush.groovy
+++ jenkins/BuildAndPush.groovy
@@ -4,20 +4,20 @@ pipeline {
         maven '3.8.2'
         jdk 'java8'
     }
-    stages{
-        stage('Build'){
-            steps{
+    stages {
+        stage('Build') {
+            steps {
                 bat 'mvn clean package'
             }
         }
-        stage('Docker build'){
-            steps{
+        stage('Docker build') {
+            steps {
                 bat 'docker build . --tag kampaii53/aws:latest'
             }
         }
-        stage('Docker push'){
-            steps{
-                withDockerRegistry([ credentialsId: "f42db408-f8db-4e38-a1cd-48caa7c67262", url: "" ]) {
+        stage('Docker push') {
+            steps {
+                withDockerRegistry([credentialsId: "f42db408-f8db-4e38-a1cd-48caa7c67262", url: ""]) {
                     bat "docker push kampaii53/aws:latest"
                 }
             }
