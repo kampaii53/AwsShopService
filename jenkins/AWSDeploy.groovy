067c418c0b7ab6a728be2c2aafa982b3017739a0
Kirill_Kuznetsov1 <Chfysqgfhjkm53>
2021-08-15 16:23:51 +0500

    added aws script

--- jenkins/AWSDeploy.groovy
+++ jenkins/AWSDeploy.groovy
@@ -1,1 +1,14 @@
-aws --version
+pipeline{
+    agent any
+    tools {
+        maven '3.8.2'
+        jdk 'java8'
+    }
+    stages {
+        stage('Build') {
+            steps {
+                bat 'aws --version'
+            }
+        }
+    }
+}
