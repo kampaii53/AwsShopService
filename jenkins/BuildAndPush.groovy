8e7e42e4511de31adfbdce4ab7720c05003c2611
Kirill_Kuznetsov1 <Chfysqgfhjkm53>
2021-08-15 16:22:47 +0500

    added aws script

--- jenkins/BuildAndPush.groovy
+++ jenkins/BuildAndPush.groovy
@@ -12,13 +12,13 @@ pipeline {
         }
         stage('Docker build') {
             steps {
-                bat 'docker build . --tag kampaii53/aws:latest'
+                bat 'docker build . --tag kampaii53/aws-shop:latest'
             }
         }
         stage('Docker push') {
             steps {
                 withDockerRegistry([credentialsId: "f42db408-f8db-4e38-a1cd-48caa7c67262", url: ""]) {
-                    bat "docker push kampaii53/aws:latest"
+                    bat "docker push kampaii53/aws-shop:latest"
                 }
             }
         }
