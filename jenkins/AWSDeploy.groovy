fe03f3ec189bd8b676f7cbe7fbee646e4496983b
Kirill_Kuznetsov1 <Chfysqgfhjkm53>
2021-08-16 11:28:22 +0500

    aws cloudformation

--- jenkins/AWSDeploy.groovy
+++ jenkins/AWSDeploy.groovy
@@ -4,9 +4,6 @@ pipeline {
         maven '3.8.2'
         jdk 'java8'
     }
-    environment {
-        USER_CREDENTIALS = credentials('f42db408-f8db-4e38-a1cd-48caa7c67262')
-    }
     stages {
         stage('Build') {
             steps {
