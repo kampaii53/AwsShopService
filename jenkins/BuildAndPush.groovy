daf3dac594dfdd6013aaf5869045ba246d97afed
Kirill_Kuznetsov1 <Chfysqgfhjkm53>
2021-08-16 10:32:10 +0500

    docker creds renamed

--- jenkins/BuildAndPush.groovy
+++ jenkins/BuildAndPush.groovy
@@ -17,7 +17,7 @@ pipeline {
         }
         stage('Docker push') {
             steps {
-                withDockerRegistry([credentialsId: "f42db408-f8db-4e38-a1cd-48caa7c67262", url: ""]) {
+                withDockerRegistry([credentialsName: "DockerHub"]) {
                     bat "docker push kampaii53/aws-shop:latest"
                 }
             }
