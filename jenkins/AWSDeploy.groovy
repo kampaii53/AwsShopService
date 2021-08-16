04bcc32577c19604bc365305e2f594a5a9cdebb4
Kirill_Kuznetsov1 <Chfysqgfhjkm53>
2021-08-16 11:21:54 +0500

    aws cloudformation

--- jenkins/AWSDeploy.groovy
+++ jenkins/AWSDeploy.groovy
@@ -6,14 +6,14 @@ pipeline {
     }
     stages {
         stage('Build') {
-            environment{
-                DOCKER_HUB_LOGIN = credentials('f42db408-f8db-4e38-a1cd-48caa7c67262')
+            environment {
+                USER_CREDENTIALS = credentials('DockerHub')
             }
             steps {
-                bat 'echo %$DOCKER_HUB_LOGIN_USR%'
-//                withAWS(credentials: 'AwsShop', region: 'us-east-1') {
-//                    bat 'aws cloudformation create-stack --stack-name awscli --template-body file://aws/example.yml'
-//                }
+                withAWS(credentials: 'AwsShop', region: 'us-east-1') {
+                    bat 'echo $USER_CREDENTIALS_USR'
+//                        bat 'aws cloudformation create-stack --stack-name awscli --template-body file://aws/example.yml'
+                }
             }
         }
     }
