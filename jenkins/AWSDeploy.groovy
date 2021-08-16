6eb757290ac173eea1c24d048439d8dd98e95dcb
Kirill_Kuznetsov1 <Chfysqgfhjkm53>
2021-08-16 12:00:56 +0500

    aws cloudformation

--- jenkins/AWSDeploy.groovy
+++ jenkins/AWSDeploy.groovy
@@ -10,7 +10,7 @@ pipeline {
                 withAWS(credentials: 'AwsShop', region: 'us-east-1') {
                     bat 'aws cloudformation create-stack --stack-name aws-shop --template-body file://aws/ecs.yaml' +
                             ' --parameters ' +
-                            'ParameterKey=SubnetID,ParameterValue=subnet-05d5c1a7255608ba5 ' +
+                            'ParameterKey=SubnetID,ParameterValue=subnet-0a0364352d537ed16 ' +
                             'ParameterKey=ServiceName,ParameterValue=aws-shop ' +
                             'ParameterKey=ServiceVersion,ParameterValue=latest ' +
                             'ParameterKey=DockerHubUserName,ParameterValue=kampaii53'
