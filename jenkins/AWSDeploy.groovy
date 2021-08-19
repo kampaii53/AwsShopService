adb11ca1336a70a82b3f21a2d83808a6c3c22ad1
Kirill_Kuznetsov1 <Chfysqgfhjkm53>
2021-08-19 22:24:11 +0500

    fix

--- jenkins/AWSDeploy.groovy
+++ jenkins/AWSDeploy.groovy
@@ -8,7 +8,7 @@ pipeline {
         stage('Build') {
             steps {
                 withAWS(credentials: 'AwsShop', region: 'us-east-1') {
-                    bat 'aws cloudformation create-stack --stack-name aws-shop --template-body file://aws/ecs-simple.yaml' +
+                    bat 'aws cloudformation create-stack --stack-name aws-shop-instances --template-body file://aws/ecs-simple.yaml' +
                             ' --parameters ' +
                             'ParameterKey=SubnetID,ParameterValue=subnet-039c2765c81c1d706 ' +
                             'ParameterKey=SecurityGroup,ParameterValue=sg-015db395e7b6d76bf ' +
