35b7b82a0a7320af3a2714fb406ced9941d5283f
Kirill_Kuznetsov1 <Chfysqgfhjkm53>
2021-08-20 11:20:47 +0500

    new scripts

--- jenkins/AWSDeploy.groovy
+++ jenkins/AWSDeploy.groovy
@@ -11,8 +11,6 @@ pipeline {
                     bat 'aws cloudformation create-stack --stack-name aws-shop-instances --template-body file://aws/ecs-simple.yaml' +
                             ' --parameters ' +
                             'ParameterKey=SubnetID,ParameterValue=subnet-039c2765c81c1d706 ' +
-                            'ParameterKey=SubnetID2,ParameterValue=subnet-0044fa4777088bb26 ' +
-                            'ParameterKey=SubnetID3,ParameterValue=subnet-0f917ea77eca849c3 ' +
                             'ParameterKey=SecurityGroup,ParameterValue=sg-015db395e7b6d76bf ' +
                             'ParameterKey=ServiceName,ParameterValue=aws-shop ' +
                             'ParameterKey=ServiceVersion,ParameterValue=latest ' +
---
+++ jenkins/AWSDeployPublicNetwork.groovy
@@ -0,0 +1,21 @@
+pipeline {
+    agent any
+    tools {
+        maven '3.8.2'
+        jdk 'java8'
+    }
+    stages {
+        stage('Build') {
+            steps {
+                withAWS(credentials: 'AwsShop', region: 'us-east-1') {
+                    bat 'aws cloudformation create-stack ' +
+                        '--stack-name aws-shop-basic-network ' +
+                        '--template-body file://aws/single-public-network.yml ' +
+                        '--capabilities CAPABILITY_IAM'
+                    bat 'aws cloudformation wait stack-create-complete ' +
+                        '--stack-name stratospheric-basic-network '
+                }
+            }
+        }
+    }
+}
