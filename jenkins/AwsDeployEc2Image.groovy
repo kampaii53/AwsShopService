f72814bafe85c08dc06933d01d2253aff723e137
Kirill_Kuznetsov1 <Chfysqgfhjkm53>
2021-08-25 14:52:45 +0500

    ec2 edit

--- jenkins/AwsDeployEc2Image.groovy
+++ jenkins/AwsDeployEc2Image.groovy
@@ -10,12 +10,7 @@ pipeline {
                 withAWS(credentials: 'AwsShop', region: 'us-east-1') {
                     bat 'aws cloudformation create-stack ' +
                             '--stack-name aws-shop-basic-service ' +
-                            '--template-body file://aws/docker-ec2-instance.yaml ' +
-                            '--parameters ' +
-                            'ParameterKey=NetworkStackName,ParameterValue=aws-shop-basic-network ' +
-                            'ParameterKey=ServiceName,ParameterValue=aws-shop-app ' +
-                            'ParameterKey=ImageUrl,ParameterValue=docker.io/kampaii53/aws-shop:latest ' +
-                            'ParameterKey=ContainerPort,ParameterValue=8080'
+                            '--template-body file://aws/docker-ec2-instance.yaml'
                     bat 'aws cloudformation wait stack-create-complete ' +
                             '--stack-name aws-shop-basic-service'
                 }
