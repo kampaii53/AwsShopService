89da7b1e23fc6599f7869a0e12d1ebd473351ab8
Kirill_Kuznetsov1 <Chfysqgfhjkm53>
2021-08-27 20:29:14 +0500

    fix

--- app/src/main/java/ru/kampaii/shop/configuration/AWSConfig.java
+++ app/src/main/java/ru/kampaii/shop/configuration/AWSConfig.java
@@ -1,13 +1,11 @@
 package ru.kampaii.shop.configuration;
 
 import com.amazonaws.services.sqs.AmazonSQSAsync;
-import io.awspring.cloud.context.config.annotation.EnableContextInstanceData;
 import io.awspring.cloud.messaging.core.QueueMessagingTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableContextInstanceData
 public class AWSConfig {
 //
 //    @Bean
