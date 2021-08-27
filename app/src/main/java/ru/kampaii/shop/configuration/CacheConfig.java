5326a7318d7d0735423f3d2c8bfa823959553acb
Kirill_Kuznetsov1 <Chfysqgfhjkm53>
2021-08-27 23:08:29 +0500

    added elasticache

---
+++ app/src/main/java/ru/kampaii/shop/configuration/CacheConfig.java
@@ -0,0 +1,9 @@
+package ru.kampaii.shop.configuration;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+}
