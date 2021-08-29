5386cbfe71828704e7a6933af00063dcc584c210
Kirill_Kuznetsov1 <Chfysqgfhjkm53>
2021-08-29 17:14:40 +0500

    approach 2

--- shopping-cart-service/src/main/java/ru/kampaii/cartservice/Application.java
+++ shopping-cart-service/src/main/java/ru/kampaii/cartservice/Application.java
@@ -2,13 +2,8 @@ package ru.kampaii.cartservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.aws.jdbc.config.annotation.EnableRdsInstance;
 
 @SpringBootApplication
-@EnableRdsInstance(dbInstanceIdentifier="cart-service-db", databaseName="${cloud.aws.rds.cart-service-db.databaseName}",
-        username="${cloud.aws.rds.cart-service-db.username}", password="${cloud.aws.rds.cart-service-db.password}")
-@EntityScan
 public class Application {
 
     public static void main(String[] args) {
