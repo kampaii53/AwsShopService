4f245c68f70c57b5c7546dec3e6d5ccd8fb7eb3a
Kirill_Kuznetsov1 <Chfysqgfhjkm53>
2021-08-29 23:28:29 +0500

    added lambda support

--- lambda/src/main/java/ru/kampaii/AwsShopLambdaHandler.java
+++ lambda/src/main/java/ru/kampaii/AwsShopLambdaHandler.java
@@ -1,16 +1,18 @@
 package ru.kampaii;
 
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClientBuilder;
 
 public class AwsShopLambdaHandler implements RequestHandler<SQSEvent, Void> {
     @Override
     public Void handleRequest(SQSEvent event, Context context) {
-        LambdaLogger logger = context.getLogger();
+        AmazonSNS client = AmazonSNSClientBuilder.standard().build();
         for (SQSEvent.SQSMessage msg : event.getRecords()) {
-            logger.log(msg.getBody());
+            //client.publish()
+            client.publish("arn:aws:sns:us-east-1:378642390019:AwsShopTopic", msg.getBody());
         }
         return null;
     }
