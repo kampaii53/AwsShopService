1f6ae121b8b7ce767601b303bd85d081672cd1e1
Kirill_Kuznetsov1 <Chfysqgfhjkm53>
2021-08-29 15:10:04 +0500

    fix

--- shopping-cart-service/src/main/java/ru/kampaii/cartservice/Application.java
+++ shopping-cart-service/src/main/java/ru/kampaii/cartservice/Application.java
@@ -3,10 +3,11 @@ package ru.kampaii.cartservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.cloud.aws.jdbc.config.annotation.EnableRdsInstance;
 
 @SpringBootApplication
-@EnableJpaRepositories
+@EnableRdsInstance(dbInstanceIdentifier="cart-service-db", databaseName="${cloud.aws.rds.cart-service-db.databaseName}",
+        username="${cloud.aws.rds.cart-service-db.username}", password="${cloud.aws.rds.cart-service-db.password}")
 @EntityScan
 public class Application {
 
