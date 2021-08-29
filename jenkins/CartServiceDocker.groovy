f8094fdb80d6c95d0fc8e5ce29c332bafc44c830
Kirill_Kuznetsov1 <Chfysqgfhjkm53>
2021-08-29 23:46:57 +0500

    fix

--- jenkins/CartServiceDocker.groovy
+++ jenkins/CartServiceDocker.groovy
@@ -14,7 +14,8 @@ pipeline {
             steps {
                 withAWS(credentials: 'AwsShop', region: 'us-east-1') {
                     bat "aws lambda create-function --function-name AwsShopLambda --zip-file" +
-                            " fileb://lambda/target/lambda-1.0-SNAPSHOT.jar"
+                            " fileb://lambda/target/lambda-1.0-SNAPSHOT.jar" +
+                            " --role arn:aws:iam::378642390019:instance-profile/KampaiiEc2Admin"
                 }
             }
         }
